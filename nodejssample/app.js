11e3a9791d646ad984a4cbf681a68e5a60da0fe8
Giang Nam <namle@kinssolutions.net>
2017-03-14 15:52:07 +0700

    asd

--- nodejssample/app.js
+++ nodejssample/app.js
@@ -218,8 +218,65 @@ app.use(function(err, req, res, next) {
     });
 });
 
-var server = app.listen(3000, function() {
-  console.log('Express server listening on port ' + server.address().port);
+// var server = app.listen(3000, function() {
+//   console.log('Express server listening on port ' + server.address().port);
+// });
+
+
+var leStore = require('le-store-certbot').create({
+  configDir: '/etc/letsencrypt',          // or /etc/letsencrypt or wherever
+  privkeyPath: ':configDir/live/myapp.techsfeed.com/privkey.pem',          //
+  fullchainPath: ':configDir/live/myapp.techsfeed.com/fullchain.pem',      // Note: both that :configDir and :hostname
+  certPath: ':configDir/live/myapp.techsfeed.com/cert.pem',                //       will be templated as expected by
+  chainPath: ':configDir/live/myapp.techsfeed.com/chain.pem',              //       node-letsencrypt
+  workDir: '/etc/letsencrypt/var/lib',
+  logsDir: '/etc/letsencrypt/var/log',
+  webrootPath: '/etc/letsencrypt/srv/www/myapp.techsfeed.com/.well-known/acme-challenge',
+  debug: false
+});
+
+// returns an instance of node-letsencrypt with additional helper methods
+var lex = require('letsencrypt-express').create({
+  // set to https://acme-v01.api.letsencrypt.org/directory in production
+  //server: 'staging',
+  server: 'https://acme-v01.api.letsencrypt.org/directory',
+
+  // If you wish to replace the default plugins, you may do so here
+  //
+  challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/etc/letsencrypt/var/acme-challenges' }) },
+  store: leStore,
+
+
+  // You probably wouldn't need to replace the default sni handler
+  // See https://github.com/Daplie/le-sni-auto if you think you do
+  //, sni: require('le-sni-auto').create({})
+
+  approveDomains: approveDomains
+});
+
+function approveDomains(opts, certs, cb) {
+  // TODO - verify domain
+
+  if (certs) {
+     opts.domains = certs.altnames;
+   }
+  else {
+     opts.email = 'namlgiang@gmail.com';
+     opts.agreeTos = true;
+  }
+
+  cb(null, { options: opts, certs: certs });
+}
+
+// app.use(express.static('public'));
+
+// handles acme-challenge and redirects to http
+require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
+  console.log("Listening for ACME http-01 challenges on", this.address());
+});
+
+server = require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
+  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
 });
 
 module.exports = app;
