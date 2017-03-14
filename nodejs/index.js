ac711449e7f7058ae1a7dd7aa92cc332f061359d
Giang Nam <namle@kinssolutions.net>
2017-03-14 13:26:05 +0700

    asd

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,18 @@
+var express = require('express');
+var app = express();
+
 var config = require("./config.js");
 var Shopify = require('shopify-api-node');
 var shopify = new Shopify({
   shopName: "namdevstore",
   apiKey: config.api_key,
   password: config.secret
+});
+
+app.get("/", function(req, res) {
+  res.send("Hello");
+});
+
+app.listen("3000", function() {
+  console.log("Running on 3000");
 });
